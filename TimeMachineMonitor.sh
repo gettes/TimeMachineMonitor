de50d92f71a9d424268dccf044c9a57ff655caa8
Michael Gettes <gettes@gmail.com>
2019-08-17 15:20:32 -0400

    lots of fine tuning

--- TimeMachineMonitor.sh
+++ TimeMachineMonitor.sh
@@ -8,72 +8,83 @@
 # this script works on High Sierra and beyond as "log stream" is needed now - logs used to be files.
 # This does handle multiple network volumes and odd timing conditions
 # No privs needed.  Execcute as login item using TimeMachineMonitor.app
+#
+# to see TimeMachine and this monitor logging
+# log stream --style syslog  --info --predicate '(senderImagePath contains[cd] "TimeMachine") || (eventMessage contains "TimeMachineMonitor")'
 
-DATE() { /bin/date "+%Y-%m-%d %H:%M:%S" ; }
-INTERVAL=5	# how often to look for stuck volumes
-READTIMEOUT=5
+INTERVAL=10
+READTIMEOUT=9
 APP="TimeMachineMonitor"
 SNAP="localsnapshots"
 TMvol="/Volumes/com.apple.TimeMachine"
 LOGFILTER='processImagePath contains "backupd" and subsystem beginswith "com.apple.TimeMachine"'
 DO_FORCE_UNMOUNT=0
 PIPE="/tmp/TMpipe.$$"
-if [ "$1" != "" ]; then
-	DO_FORCE_UNMOUNT=1
-fi
+LOGGER() { /usr/bin/logger -s -p local0.info $APP: $1 ; }
+if [ "$1" != "" ]; then DO_FORCE_UNMOUNT=1; fi
 
 trap 'forceUnmount' SIGUSR1
-trap 'echo "$(DATE) Ouch."; /bin/kill $LOGPID $SIGPID; /bin/rm -f $PIPE; exit' SIGQUIT SIGTERM SIGINT SIGILL SIGBUS SIGSEGV
-trap 'Force 1; echo "HUP FORCE=$DO_FORCE_UNMOUNT"' SIGHUP
-
-{ set -e
-  sleep $INTERVAL
-  while (true) do
-	sleep $INTERVAL
-	/bin/kill -USR1 $$ 2>/dev/null
-  done } &
-SIGPID=$!	# save for later killing on term
-
-/usr/bin/mkfifo $PIPE # named pipe for asynch handling
-/usr/bin/log stream --style syslog --info --predicate "$LOGFILTER" > $PIPE &
-LOGPID=$!	# keep track for later killing on term
-exec < $PIPE	# push named pipe to stdin here
+trap 'LOGGER "End"; kill $LOGPID $SIGPID; /bin/rm -f $PIPE; exit 0' SIGQUIT SIGTERM SIGINT
+trap 'Force $(( $DO_FORCE_UNMOUNT + 1 )) ; LOGGER "+1 FORCE=$DO_FORCE_UNMOUNT"' SIGUSR2
+trap 'Force $(( $DO_FORCE_UNMOUNT - 1 )) ; LOGGER "-1 FORCE=$DO_FORCE_UNMOUNT"' SIGHUP
+trap 'LOGGER "Status FORCE=$DO_FORCE_UNMOUNT"' SIGINFO
 
 Force() { DO_FORCE_UNMOUNT=$1; }
 forceUnmount() {
-	if [ $DO_FORCE_UNMOUNT -eq 1 ]; then
+	if [ $DO_FORCE_UNMOUNT -ge 2 ]; then LOGGER "forceUnmount: FORCE=$DO_FORCE_UNMOUNT" ; fi
+	if [ $DO_FORCE_UNMOUNT -ge 1 ]; then
                 TMvols=( $TMvol.* )
 		for vol in "${TMvols[@]}"; do
+			if [ $DO_FORCE_UNMOUNT -ge 2 ]; then LOGGER "forceUnmount: Vol=$vol" ; fi
 			if [ "$vol" != "$TMvol.$SNAP" -a "$vol" != "$TMvol.*" ]; then
-				echo "$(DATE) $APP: Force `/usr/sbin/diskutil unmountDisk force \"$vol\"`"
+				trytype=""
+				try=$(/usr/sbin/diskutil unmountDisk "$vol")
+				if [ $? -ne 0 ]; then
+					trytype="force "
+					try=$(/usr/sbin/diskutil unmountDisk force "$vol")
+				fi
+				LOGGER "$trytype$try"
 			fi
 		done
-		Force 0
+		if [ $DO_FORCE_UNMOUNT -le 2 ]; then Force 0; fi
 	fi
 }
 
-echo "$(DATE) $APP:$DO_FORCE_UNMOUNT Monitor Start"
+# subshell signals parent to spark forceUnmount handling
+{ set -e ; sleep $INTERVAL ; while (true) do sleep $INTERVAL ; kill -USR1 $$ 2>/dev/null ; done } &
+SIGPID=$!	# save for later killing on exit
+/usr/bin/mkfifo $PIPE # named pipe for asynch handling
+/usr/bin/log stream --style syslog --info --predicate "$LOGFILTER" > $PIPE &
+LOGPID=$!	# keep track for later killing on term
+exec < $PIPE	# push named pipe to stdin here for while read loop below
+
+LOGGER "Start"
 while (true) do
-	while read -t $READTIMEOUT -r line
+	while read -t $READTIMEOUT -r logmsg 
 	do
-		case $line in
+		case $logmsg in
+		*'Attempting to mount '*)Force 0
+					LOGGER "Looking for backup disk..." ;;
 		*'Checking size of '*)	Force 0
-					echo "$(DATE) $APP: Preparing backup..." ;;
-		*'Backing up to '*)	Force 0
-					echo "$(DATE) $APP: Backup Starting..." ;;
-		*'Cancellation timed out'*)	Force 1
-						echo "$(DATE) $APP: Timeout: Cancellation" ;;
-		*'Verifying backup disk image'*) Force 0
-						 echo "$(DATE) $APP: Verify Started" ;;
-		*'Backup verification '*) Force 0
-					  echo "$(DATE) $APP: Verify Ended" ;;
+					LOGGER "Preparing backup..." ;;
+		*'Will copy ('*)	Force 0
+					LOGGER "Backup starting..." ;;
+		*'Completed snapshot:'*)Force 0
+					LOGGER "Finishing backup..." ;;
+		*'Starting post-backup'*)Force 0
+					LOGGER "Cleaning up..." ;;
+		*'Verifying backup '*)	Force 0
+				 	LOGGER "Verify started" ;;
+		*'Backup verification'*)Force 0
+					LOGGER "Verify ended" ;;
 		*'Backup completed'*) 	Force 0
-					echo "$(DATE) $APP: Backup Completed" ;;
+					LOGGER "Backup completed" ;;
 		*'Backup canceled'*)	Force 0
-					echo "$(DATE) $APP: Backup Cancelled" ;;
-		*'Failed to unmount'*)	Force 1 ;;
-		*'Failed to eject'*)	Force 1 ;;
-		*'Ejected '*' from '*)	Force 0 ;;
+					LOGGER "Stopping..." ;;
+		*'Cancellation timed out'*)	Force 1 ;;
+		*"Failed to unmount '$TMvol."*) Force 1 ;;
+		*"Unmounted '$TMvol."*)		Force 1 ;; # Force 1 to catch other vols still mounted
 		esac
 	done 
 done
+
