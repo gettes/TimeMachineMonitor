195f82d6631635726510c5e0d76c82cf370c05da
Michael Gettes <gettes@gmail.com>
2019-08-18 10:22:29 -0400

    status app clean up

--- VolumeStatus.sh
+++ VolumeStatus.sh
@@ -8,28 +8,39 @@ TMvol="/Volumes/com.apple.TimeMachine"
 Vols="/Volumes/"
 DATE() { /bin/date "+%Y-%m-%d %H:%M:%S" ; }
 
-echo "Volumes mounted @ $(DATE)"
-echo " "
+echo "DISABLED|Volumes mounted @ $(DATE)"
+echo "----"
 TMvols=( $Vols* )
 TMmounted=0
 for vol in "${TMvols[@]}"; do
-	TM="        "
-	[[ $vol =~ "Time Machine Backups" ]] && TM="TM vol: " && TMmounted=1
-	[[ $vol =~ ^$TMvol ]] && TM="TM vol: " && TMmounted=1
-	echo "$TM$vol"
+	TM="    "
+	[[ $vol =~ "Time Machine Backups" ]] && TM="TM: " && TMmounted=1
+	[[ $vol =~ ^$TMvol ]] && TM="TM: " && TMmounted=1
+	echo "DISABLED|$TM$vol"
 done
 
+echo "----"
 if [ $TMmounted -eq 1 ]; then
-	echo " "
-	echo "**** Time Machine volumes still mounted"
-	echo "     DO NOT close laptop or disconnect Time Machine devices"
-	echo " "
-	echo "     Wait for Time Machine to finish"
-	echo " "
+cat <<HERE
+DISABLED|**** Time Machine volumes still mounted
+DISABLED|     DO NOT close laptop or disconnect Time Machine devices
+DISABLED|
+DISABLED|     Wait for Time Machine to finish!
+HERE
 else
-	echo " "
-	echo "  Time Machine doesn't appear to be running now"
-	echo "     It appears to be safe to close your laptop"
-	echo "     and/or disconnect Time Machine devices"
-	echo " "
+cat <<HERE
+DISABLED|  Time Machine doesn't appear to be running now
+DISABLED|     It should be safe to close your laptop
+DISABLED|     and/or disconnect Time Machine devices
+HERE
 fi
+
+exit
+
+cat <<HERE
+----
+MENUITEMICON|http://sveinbjorn.org/images/andlat.png|Or even a URL!
+SUBMENU|Title|Item1|Item2|Item3
+DISABLED|SUBMENU|Item1|Item4|Item5
+HERE
+
