fa0567c7123844618dedaf07ab6444c68a912fec
Michael Gettes <gettes@gmail.com>
2019-08-22 22:06:13 -0400

    bells and whistles
    look for monitor running - run monitor app if not
    allow opening log app
    open github site

--- TimeMachineStatus.sh
+++ TimeMachineStatus.sh
@@ -9,6 +9,10 @@ Vols="/Volumes/"
 NETRE='^(.+) on (.+) \((.+),.*'
 DATE() { /bin/date "+%Y-%m-%d %H:%M:%S" ; }
 
+[[ $* =~ "Open TimeMachineLog" ]] && /usr/bin/open -b org.gettes.TimeMachineLog
+[[ $* =~ "Status: TimeMachineMonitor" ]] && /usr/bin/open -b org.gettes.TimeMachineMonitor
+[[ $* =~ "TimeMachineMonitor on GitHub" ]] && /usr/bin/open "http://github.com/gettes/TimeMachineMonitor"
+
 TMvols=( $Vols* )
 TMmounted=0
 IFSBAK=$IFS; IFS=$'\n' # change IFS so the following will work
@@ -17,8 +21,14 @@ mntvols=( $(/sbin/mount -vt afpfs,smbfs,hfs,apfs,exfat) )
 #TMvols=( $(/bin/cat ./vol.out) ) # for debugging
 IFS=$IFSBAK
 
-echo "DISABLED|TimeMachineStatus: Volumes mounted" # @ $(DATE)"
-echo "----"
+MonCount=$(/bin/ps axw | /usr/bin/grep TimeMachineMonitor.app/Contents/Resources/script | /usr/bin/grep -v /usr/bin/grep | /usr/bin/wc -l | /usr/bin/awk '{print $1}')
+MonStatus="NOT Running" ; MonActive=""
+[[ $MonCount -eq 2 ]] && MonStatus="Running" && MonActive="DISABLED|"
+/bin/cat <<HERE
+${MonActive}MENUITEMICON|AppIcon.icns|Status: TimeMachineMonitor is $MonStatus
+MENUITEMICON|AppIcon.icns|Open TimeMachineLog
+----
+HERE
 for vol in "${TMvols[@]}"; do
 	ismntvol=0
 	if [ ${#mntvols[@]} -gt 0 ]; then
@@ -28,7 +38,8 @@ for vol in "${TMvols[@]}"; do
 			if [[ $mntvol =~ $NETRE ]]; then
 				mntvol=${BASH_REMATCH[2]}
 			fi
-			[[ $mntvol != "/" && $mntvol =~ ^$vol ]] && ismntvol=1 && break
+			#[[ $mntvol != "/" && $mntvol =~ ^$vol ]] && ismntvol=1 && break
+			[[ $mntvol =~ ^$vol ]] && ismntvol=1 && break
 		done
 	fi
 	TM="    "; TMmark=" -> "
@@ -39,9 +50,9 @@ for vol in "${TMvols[@]}"; do
 	echo "DISABLED|$TM$vol"
 done
 
-echo "----"
 if [ $TMmounted -eq 1 ]; then
-cat <<HERE
+/bin/cat <<HERE
+----
 DISABLED|
 DISABLED|**** Time Machine appears to be active
 DISABLED|
@@ -51,19 +62,23 @@ DISABLED|     Wait for Time Machine to finish!
 DISABLED|
 HERE
 else
-cat <<HERE
+/bin/cat <<HERE
+----
 DISABLED|  Time Machine doesn't appear to be running
 DISABLED|  Should now be safe to eject Time Machine devices
 DISABLED|    and/or close your laptop
 HERE
 fi
 
+/bin/cat <<HERE
+----
+MENUITEMICON|AppIcon.icns|TimeMachineMonitor on GitHub
+HERE
+
 exit
 
 cat <<HERE
 ----
-MENUITEMICON|http://sveinbjorn.org/images/andlat.png|Or even a URL!
 SUBMENU|Title|Item1|Item2|Item3
-DISABLED|SUBMENU|Item1|Item4|Item5
 HERE
 
