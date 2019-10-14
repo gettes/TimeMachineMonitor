a65b7e6631600af3975b065e2bcbcbccc4be16f6
Michael Gettes <gettes@gmail.com>
2019-10-13 20:18:40 -0400

    Catalina support and lots of fixes

--- TimeMachineMonitor.sh
+++ TimeMachineMonitor.sh
@@ -10,27 +10,42 @@
 # No privs needed.  Execcute as login item using TimeMachineMonitor.app
 #
 
+DEBUG=0
 INTERVAL=10
-READTIMEOUT=30
+READTIMEOUT=10
+OSX_VER=$(/usr/bin/sw_vers -productVersion)
 APP="TimeMachineMonitor"
 SNAP="localsnapshots"
 TMvol="/Volumes/com.apple.TimeMachine"
+TMvol2="/Volumes/.timemachine"
 LOGFILTER='processImagePath contains "backupd" and subsystem beginswith "com.apple.TimeMachine"'
+LOGCOMMAND="/usr/bin/log stream --style syslog --info --predicate"
 DO_FORCE_UNMOUNT=0
 PIPE=/tmp/TMpipe.$$
 DISKUTIL="/usr/sbin/diskutil"
-STATUSFILE="/tmp/.$APP"
-LOGGER() { /usr/bin/logger -s -p local0.info $APP: $1; echo "$(/bin/date -j -f '%a %b %d %T %Z %Y' "$(/bin/date)" '+%s') $1" > $STATUSFILE ; }
+STATUSFILE="/tmp/.TimeMachineMonitor"
+LOGGER() { /usr/bin/logger -s -p local0.info $APP: $1; echo "$(/bin/date -j -f '%a %b %d %T %Z %Y' "$(/bin/date)" '+%s') $1" >> $STATUSFILE ; }
 NETRE='^(.+) on (.+) \((afpfs|smbfs),.*'
 MNTRE='^(.+) on (.+) \(.+'
-if [ "$1" != "" ]; then DO_FORCE_UNMOUNT=1; fi
 self=$$
 running=`: ; pid=$(bash -c 'echo $PPID'); /bin/ps ax | /usr/bin/grep "bash $0" | /usr/bin/egrep -v "grep|$self|$pid" | /usr/bin/awk '{print $1}'`
+
+if [ -n "$1" ]; then		# set up debugging
+	DEBUG=1
+	DISKUTIL="echo"
+	running=""
+	INTERVAL=2
+	READTIMEOUT=3
+	LOGCOMMAND="/usr/bin/tail -f -n 100000000 $1"
+	LOGFILTER="''"
+fi
+
 for pid in "$running"; do kill $pid 2>/dev/null; done
+[[ $DEBUG -ne 0 ]] && echo "debugging DISKUTIL=$DISKUTIL"
 
 trap 'forceUnmount' SIGUSR1
 trap 'LOGGER "Caught SIGILL - Restarting" ; Restart' SIGILL
-trap 'CleanUp ; exit 0' SIGQUIT SIGTERM SIGINT SIGHUP
+trap 'CleanUp ; exit 0' SIGTERM SIGHUP SIGQUIT
 trap 'Force $(( $DO_FORCE_UNMOUNT + 1 )) ; LOGGER "+1 FORCE=$DO_FORCE_UNMOUNT"' SIGUSR2
 trap 'Force $(( $DO_FORCE_UNMOUNT - 1 )) ; LOGGER "-1 FORCE=$DO_FORCE_UNMOUNT"' SIGBUS
 trap 'LOGGER "Status FORCE=$DO_FORCE_UNMOUNT"' SIGINFO
@@ -46,7 +61,7 @@ Restart() {
 	/usr/bin/open -b org.gettes.TimeMachineMonitor &
 	exit 0
 }
-Force() { DO_FORCE_UNMOUNT=$1; }
+Force() { DO_FORCE_UNMOUNT=$1; [[ $DEBUG -ne 0 ]] && LOGGER "Force = $DO_FORCE_UNMOUNT" ; }
 forceUnmount() {
 	IFSBAK=$IFS; IFS=$'\n' # change IFS so the following will work
 	netvols=($(/sbin/mount -vt smbfs,afpfs))  # TM vols are only on afp/smb network volumes
@@ -54,17 +69,24 @@ forceUnmount() {
 	IFS=$IFSBAK
 	if [ $DO_FORCE_UNMOUNT -ge 2 ]; then LOGGER "forceUnmount: FORCE=$DO_FORCE_UNMOUNT" ; fi
 	if [ $DO_FORCE_UNMOUNT -ge 1 ]; then
-                TMvols=( $TMvol.* )
+                TMvols=( /Volumes/* $TMvol2/* )
 		for vol in "${TMvols[@]}"; do
-			if [ $DO_FORCE_UNMOUNT -ge 2 ]; then LOGGER "forceUnmount: Vol=$vol" ; fi
-			if [ "$vol" != "$TMvol.$SNAP" -a "$vol" != "$TMvol.*" ]; then
+		#for netvol_cnt in $(seq 0 $((${#netvols[@]} - 1))); do	# 10.15
+			# [[ $netvol_cnt -lt 0 ]] && continue
+			# vol="${netvols[$netvol_cnt]}"
+			# if [[ $vol =~ $MNTRE ]]; then vol=${BASH_REMATCH[2]}; fi
+			[[ $DO_FORCE_UNMOUNT -ge 2 ]] && LOGGER "forceUnmount: Vol=$vol"
+			# if [ "$vol" != "$TMvol.$SNAP" -a "$vol" != "$TMvol.*" ]; then
+			if ( [[ $vol =~ ^$TMvol2.+ ]] || [[ $vol =~ ^$TMvol.+ ]] ) ; then	# 10.15
 				isnetvol=0
 				for cnt in $(seq 0 $((${#netvols[@]} - 1))); do
+					[[ $cnt -lt 0 ]] && continue
 					netvol="${netvols[$cnt]}"
 					if [[ $netvol =~ $NETRE ]]; then
 						mntvol=${BASH_REMATCH[2]}
 					fi
-					[[ $vol = $mntvol ]] && isnetvol=1
+					# [[ $vol =~ $mntvol ]] && isnetvol=1
+					[[ $mntvol =~ ^$vol ]] && isnetvol=1 && vol=$mntvol
 				done
 				[[ $isnetvol -eq 0 ]] && continue # only try unmount on netvols
 				trytype="netvol=$isnetvol "
@@ -98,60 +120,100 @@ forceUnmount() {
 		if [ $DO_FORCE_UNMOUNT -le 2 ]; then Force 0; LOGGER "Backup ended."; fi
 	fi
 }
-childSignalHandler() {
+SignalHandler() {
 	kill -0 $SIGPID 2>/dev/null
 	if [ $? -ne 0 ]; then echo "sig ($SIGPID) dead!"; startSignaler; fi
+	if [ $? -ne 0 ]; then 
+		LOGGER "sig ($SIGPID) dead! (DEBUG=$DEBUG)"
+		kill -0 $SIGPID 
+		wait $SIGPID
+	fi
 	kill -0 $LOGPID 2>/dev/null
-	if [ $? -ne 0 ]; then echo "log ($LOGPID) dead!"; startLogstream; fi
+	if [ $? -ne 0 ]; then 
+		LOGGER "log ($LOGPID) dead! (DEBUG=$DEBUG $LOGCOMMAND $LOGFILTER)"
+		kill -0 $LOGPID 
+		wait $LOGPID
+		[[ $DEBUG -eq 0 ]] && CleanUp; exit && startLogstream
+	fi
 }
 startSignaler() {
 	# subshell signals parent to spark forceUnmount handling
-	{ set -e ; sleep $INTERVAL ; while (true) do sleep $INTERVAL ; kill -USR1 $$ 2>/dev/null ; done } &
+	{ set -e ; while (true) do sleep $INTERVAL ; kill -USR1 $$ 2>/dev/null ; done } &
 	SIGPID=$!	# save for later killing on exit
 }
 startLogstream() {
 	/bin/rm -f $PIPE
 	/usr/bin/mkfifo $PIPE # named pipe for asynch handling
-	/usr/bin/log stream --style syslog --info --predicate "$LOGFILTER" > $PIPE &
+	$LOGCOMMAND "$LOGFILTER" > $PIPE &
 	LOGPID=$!	# keep track for later killing on term
 	exec < $PIPE	# push named pipe to stdin here for while read loop below
 }
 
-startSignaler
 startLogstream
+startSignaler
 
-set -m
-trap 'childSignalHandler' SIGCHLD
+#set -m
+#trap 'SignalHandler' SIGCHLD
 
-LOGGER "Start ($self)"
+LOGGER "Start ($self) $OSX_VER"
+[[ $DEBUG -ne 0 ]] && Force 2
 while (true) do
 	while read -t $READTIMEOUT -r logmsg 
 	do
 		case $logmsg in
+		*'Starting '*'backup')	Force 0
+					/bin/rm -f $STATUSFILE
+					LOGGER "Started" ;;
 		*'Attempting to mount '*)Force 0
 					LOGGER "Looking for backup disk..." ;;
 		*'Checking for runtime '*)Force 0
 					LOGGER "Preparing backup..." ;;
 		*'Will copy ('*)	Force 0
 					LOGGER "Backing up..." ;;
+		*'need to be backed up from all sources'*) Force 0 # MacOS 10.15
+					LOGGER "Backing up..." ;;
 		*'Completed snapshot:'*)Force 0
 					LOGGER "Finishing backup..." ;;
+		*'Completed backup:'*)Force 0 # MacOS 10.15
+					LOGGER "Finishing backup..." ;;
 		*'Starting post-backup'*)Force 0
+					LOGGER "Cleaning up..." ;;
+		*'Finished copying recovery system'*)Force 0 # MacOS 10.15
 					LOGGER "Cleaning up..." ;;
 		*'Verifying backup '*)	Force 0
 				 	LOGGER "Verify started" ;;
 		*'Backup verification'*)Force 0
 					LOGGER "Verify ended" ;;
 		*'Backup completed'*) 	Force 0
-					LOGGER "Backup completed" ;;
+					LOGGER "Backup almost completed" ;;
 		*'Backup canceled'*)	Force 0
+					LOGGER "Stopping..."
+					[[ $OSX_VER == "10.15" ]] && Force 1
+					;;
+		*'Backup cancel was requested.'*) Force 0
 					LOGGER "Stopping..." ;;
+		*' Thinning '*) 	Force 0
+					LOGGER "Thinning Backup Volume..." ;;
 		*'Cancellation timed out'*)	Force 1 ;;
-		*'Failed to unmount snapshot:'*)LOGGER "Failure unmount snapshot" ;;
-		*"Failed to unmount '$TMvol."*) Force 1 ;;
+#		*'Pending cancel request cleared.'*)	Force 1 ;; # MacOS 10.15
+#		*'Failed to unmount snapshot:'*)LOGGER "Failure unmount snapshot" ;;
+		*"Failed to unmount '$TMvol."*) Force 1
+					LOGGER "Backup ending... monitor cleanup"
+					;;
+		*"Failed to unmount '$TMvol2"*) Force 1 
+					LOGGER "TM unmount failed (10.15)"
+					LOGGER "Backup ending... monitor cleanup"
+					[[ $DEBUG -ne 0 ]] && Force 2
+					;; # MacOS 10.15
 		*"Unmounted '$TMvol."*)		Force 1 ;; # MacOS 10.14
+		*"Unmounted '$TMvol2."*)	Force 1 ;; # MacOS 10.15
 		*"Ejected Time Machine network volume"*)Force 1 ;; # MacOS 10.13
 		esac
 	done 
+	SignalHandler
+
+	#echo "TIMEOUT: $?"
+	#/usr/bin/tmutil status | grep DestinationID
+	#echo "rc: $?"
 done
 
--- TimeMachineStatus.sh
+++ TimeMachineStatus.sh
@@ -5,9 +5,11 @@
 APP="VolumeStatus"
 SNAP="localsnapshots"
 TMvol="/Volumes/com.apple.TimeMachine"
-Vols="/Volumes/"
+TMvol2="/Volumes/.timemachine"
+Vols="/Volumes/* /Volumes/.timemachine/*"
 NETRE='^(.+) on (.+) \((.+),.*'
 DATE() { /bin/date "+%Y-%m-%d %H:%M:%S" ; }
+STATUSFILE=/tmp/.TimeMachineMonitor
 MonPIDS=$(/bin/ps axw | /usr/bin/egrep 'TimeMachineMonitor.app/Contents/Resources/script|TimeMachineMonitor.app/Contents/MacOS/TimeMachineMonitor' | /usr/bin/grep -v /usr/bin/egrep | /usr/bin/awk '{print $1}')
 
 RemoveLoginItems() {
@@ -76,7 +78,15 @@ e2='^FORCE\: (.*)$'
 [[ $* =~ $e1 ]] && /usr/sbin/diskutil eject "${BASH_REMATCH[1]}"
 [[ $* =~ $e2 ]] && /usr/sbin/diskutil unmount force "${BASH_REMATCH[1]}"
 
-TMvols=( $Vols* )
+timeRemaining=$(/usr/bin/tmutil status | /usr/bin/grep TimeRemaining)
+if [ $? -eq 0 ]; then
+	tRE='^.*= ([0-9]+);'
+	[[ "$timeRemaining" =~ $tRE ]] && timeRemaining=${BASH_REMATCH[1]}
+	printf -v timeRemaining '      about %dh %dm %ss to go;  ' $(($timeRemaining / 3600)) $((($timeRemaining / 60) % 60)) $(($timeRemaining % 60))
+else timeRemaining="                     "
+fi
+
+TMvols=( $Vols )
 TMmounted=0
 IFSBAK=$IFS; IFS=$'\n' # change IFS so the following will work
 mntvols=( $(/sbin/mount -vt afpfs,smbfs,hfs,apfs,exfat) )
@@ -84,23 +94,52 @@ mntvols=( $(/sbin/mount -vt afpfs,smbfs,hfs,apfs,exfat) )
 #TMvols=( $(/bin/cat ./vol.out) ) # for debugging
 IFS=$IFSBAK
 
-monTime="" ; monStatus=""
-if [ -f /tmp/.TimeMachineMonitor ]; then
-	read -r monTime monStatus < /tmp/.TimeMachineMonitor
-	now=$(/bin/date -j -f '%a %b %d %T %Z %Y' "$(/bin/date)" '+%s')
-	diff=$(( $now - $monTime ))
-	monHour=$(($diff / 3600)) ; monMin=$((($diff / 60) % 60)) ; monSec=$(($diff % 60))
-	monTime=": ${monHour}h ${monMin}m ago:"
-fi
+monTime="" ; monStatus=""; monTimeStart=""; monStatus2=""; st_items=""
 MonCount=$(/bin/ps axw | /usr/bin/grep TimeMachineMonitor.app/Contents/Resources/script | /usr/bin/grep -v /usr/bin/grep | /usr/bin/wc -l | /usr/bin/awk '{print $1}')
 MonStatus="******   Monitor is NOT Running   ******" ; MonActive="DISABLED|" ; MonSUB="Start Monitor"
-[[ $MonCount -eq 2 ]] && MonStatus="Monitor Running${monTime} ${monStatus}" && MonActive="DISABLED|" && MonSUB="End Monitor"
+if [ $MonCount -eq 2 -a -f "$STATUSFILE" ]; then
+	st_RE='^([0-9]+) (.+)$'
+	now=$(/bin/date -j -f '%a %b %d %T %Z %Y' "$(/bin/date)" '+%s')
+	IFSBAK=$IFS; IFS=$'\n' # change IFS so the following will work
+	st_file=( $(/bin/cat $STATUSFILE) )
+	IFS=$IFSBAK
+	[[ "${st_file[${#st_file[@]}-1]}" =~ $st_RE ]] && monTime=${BASH_REMATCH[1]} && monStatus=${BASH_REMATCH[2]}
+	[[ "${st_file[0]}" =~ $st_RE ]] && monTimeStart=${BASH_REMATCH[1]} && monStatusStart=${BASH_REMATCH[2]}
+	diff=$(( $now - $monTime ))
+	printf -v monTime ': %dh %dm ago: %s' $(($diff / 3600)) $((($diff / 60) % 60)) "$monStatus"
+	diff=$(( $now - $monTimeStart ))
+	printf -v monTimeStart '%s %dh %dm ago: %s' "$timeRemaining" $(($diff / 3600)) $((($diff / 60) % 60)) "$monStatusStart"
+	MonStatus="Monitor Running${monTime}" ; MonActive="DISABLED|" ; MonSUB="End Monitor"; MonStatus2="${monTimeStart}"
+	for st_cnt in $(seq 0 ${#st_file[@]}) ; do
+		st_time=""
+		if [[ "${st_file[$st_cnt]}" =~ $st_RE ]]; then
+			st_time=${BASH_REMATCH[1]} ; st_status=${BASH_REMATCH[2]}
+		fi
+		[[ -z "$st_time" ]] && continue
+		diff=$(( $now - $st_time ))
+		printf -v st_item '%02d:%02d:%02d %s' $(($diff / 3600)) $((($diff / 60) % 60)) $(($diff % 60)) "$st_status"
+		[[ -z "$st_items" ]] && st_items=$st_item && continue
+		st_items="$st_item|$st_items"
+	done
+	st_items="    Age      Status|$st_items"
+fi
 
+if [ $MonCount -eq 2 ]; then
+/bin/cat <<HERE
+${MonActive}MENUITEMICON|AppIcon.icns|  $MonStatus
+${MonActive}$MonStatus2
+SUBMENU|     Status History for last Backup|$st_items
+SUBMENU|     Monitor Actions|Open TimeMachineLog|$MonSUB|Install LoginItems|Remove LoginItems
+----
+HERE
+else
 /bin/cat <<HERE
 ${MonActive}MENUITEMICON|AppIcon.icns|  $MonStatus
 SUBMENU|     Monitor Actions|Open TimeMachineLog|$MonSUB|Install LoginItems|Remove LoginItems
 ----
 HERE
+fi
+
 for vol in "${TMvols[@]}"; do
 	ismntvol=0
 	if [ ${#mntvols[@]} -gt 0 ]; then
@@ -117,31 +156,33 @@ for vol in "${TMvols[@]}"; do
 	[[ $vol =~ ^$TMvol\.$SNAP ]] && continue
 	[[ $vol =~ "Time Machine Backups" ]] && TM=$TMmark && TMmounted=1
 	[[ $vol =~ ^$TMvol && $ismntvol -eq 1 ]] && TM=$TMmark && TMmounted=1
-	[[ $vol = "/Volumes/Recovery" ]] && TM=$TMmark && TMmounted=1
+	[[ $vol =~ ^$TMvol2 && $ismntvol -eq 1 ]] && TM=$TMmark && TMmounted=1
+	[[ $vol =~ ^$TMvol2 && $ismntvol -eq 0 ]] && TM=$TMmark && continue
+	[[ $vol = "/Volumes/Recovery" ]] && echo "DISABLED|$TMmark$vol" && continue
 	[[ $TMmounted -eq 1 ]] && echo "DISABLED|$TM$vol" && continue
 	[[ $TMmounted -eq 0 && $vol != "/Volumes/Macintosh HD" ]] && echo "SUBMENU|$TM$vol|EJECT: $vol|FORCE: $vol" && continue
 	echo "DISABLED|$TM$vol"
 done
 
-if [ $TMmounted -eq 1 ]; then
-/bin/cat <<HERE
-----
-DISABLED|
-DISABLED|**** Time Machine appears to be active
-DISABLED|
-DISABLED|     DO NOT close your laptop or eject Time Machine devices
-DISABLED|
-DISABLED|     Wait for Time Machine to finish!
-DISABLED|
-HERE
-else
-/bin/cat <<HERE
-----
-DISABLED|  Time Machine doesn't appear to be running
-DISABLED|  Should now be safe to eject Time Machine devices
-DISABLED|    and/or close your laptop
-HERE
-fi
+#if [ $TMmounted -eq 1 ]; then
+#/bin/cat <<HERE
+#----
+#DISABLED|
+#DISABLED|**** Time Machine appears to be active
+#DISABLED|
+#DISABLED|     DO NOT close your laptop or eject Time Machine devices
+#DISABLED|
+#DISABLED|     Wait for Time Machine to finish!
+#DISABLED|
+#HERE
+#else
+#/bin/cat <<HERE
+#----
+#DISABLED|  Time Machine doesn't appear to be running
+#DISABLED|  Should now be safe to eject Time Machine devices
+#DISABLED|    and/or close your laptop
+#HERE
+#fi
 
 /bin/cat <<HERE
 ----
